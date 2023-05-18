1e821e62efc40a242db79f1948e61662de56928c
Nicholas <n.spera98@gmail.com>
2023-05-18 17:11:31 -0400

    working bookMark some css

--- index.js
+++ index.js
@@ -5,51 +5,45 @@ const bookMarkList = document.querySelector('#bookmark-list');
 
 let bookMarks = [];
 
-addBtn.addEventListener('click', () =>{
-    const bookmarkObj = {
-        link: urlInput.value,
-        name: nameInput.value
-    }
-    bookMarks.push(bookmarkObj);
-    urlInput.value="";
-    nameInput.value="";
+addBtn.addEventListener('click', () => {
+  const bookmarkObj = {
+    link: urlInput.value,
+    name: nameInput.value,
+  };
+  bookMarks.push(bookmarkObj);
+  urlInput.value = '';
+  nameInput.value = '';
 
-    render();
+  render();
 });
 
-
-
-
-function render(){
-    const arry = bookMarks.map(makeListItem)
-    bookMarkList.innerHTML='';
-    for(const item of arry){
-        bookMarkList.append(item);
-    }
+function render() {
+  const arry = bookMarks.map(makeListItem);
+  bookMarkList.innerHTML = '';
+  for (const item of arry) {
+    bookMarkList.append(item);
+  }
 }
 
-
-function makeListItem (obj){
-    const listelm = document.createElement('li');
-    const nameField = document.createElement('span');
-    nameField.innerText = obj.name;
-    listelm.append(nameField);
-    const urlField = document.createElement('a');
-    urlField.href = obj.link;
-    urlField.innerText = obj.link;
-    listelm.append(urlField);
-    const delBtn = document.createElement('button');
-    delBtn.innerText = 'Delete Bookmark';
-    delBtn.addEventListener("click", () => {
-        deleteFunction(obj.name)
-    });
-    listelm.append(delBtn);
-    return listelm;
+function makeListItem(obj) {
+  const listelm = document.createElement('li');
+  const nameField = document.createElement('span');
+  nameField.innerText = obj.name.charAt(0).toUpperCase() + obj.name.slice(1);
+  listelm.append(nameField);
+  const urlField = document.createElement('a');
+  urlField.href = obj.link;
+  urlField.innerText = obj.link;
+  listelm.append(urlField);
+  const delBtn = document.createElement('button');
+  delBtn.innerText = 'Delete Bookmark';
+  delBtn.addEventListener('click', () => {
+    deleteFunction(obj.name);
+  });
+  listelm.append(delBtn);
+  return listelm;
 }
 
-function deleteFunction(name){
-    bookMarks = bookMarks.filter(item => item.name !== name);
-    render();
+function deleteFunction(name) {
+  bookMarks = bookMarks.filter(item => item.name !== name);
+  render();
 }
-
-
